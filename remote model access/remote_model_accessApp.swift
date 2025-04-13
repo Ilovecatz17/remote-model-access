d5ea5f246ed33fe9fbb123fe5c694dcecc72be44
tommy g <juliousrobins@icloud.com>
2025-04-13 11:02:07 -0400

    Initial Commit

---
+++ remote model access/remote_model_accessApp.swift
@@ -0,0 +1,17 @@
+//
+//  remote_model_accessApp.swift
+//  remote model access
+//
+//  Created by Thomas Grossman on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct remote_model_accessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ remote model accessTests/remote_model_accessTests.swift
@@ -0,0 +1,17 @@
+//
+//  remote_model_accessTests.swift
+//  remote model accessTests
+//
+//  Created by Thomas Grossman on 4/13/25.
+//
+
+import Testing
+@testable import remote_model_access
+
+struct remote_model_accessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
